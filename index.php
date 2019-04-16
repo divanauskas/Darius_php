dd61a1cbf4594aab9a880d0eb03b2a63ec4785b4
Your NameDarius <didariusivanauskas@gmail.com>
2019-04-16 15:28:59 +0300

    Trecias commit

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html>
     <head>
-        <title>Aš valdau HTML!</title>
+        <title>Aš valdau HTML <?php print 'ir PHP';?></title>
     </head>
     <body>
-        <h1>Vardas - HTML asas</h1>
-        <p>Moku kurti statinį turinį</p>
-        
-        
+        <h1>Vardas - HTML <?php print 'ir PHP';?> asas</h1>
+        <p>Kuriu dinaminį turinį.</p>
+        <p>Atidaryta:</p> 
+        <?php print date("l jS \of F Y h:i:s A");?>    
     </body>
     
 </html>
