436769ef4a6e9896cb118839a607085a4508dae6
Your NameDarius <didariusivanauskas@gmail.com>
2019-04-16 17:33:49 +0300

    septintas commit

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <body>
         <h1>Dainų Tekstai</h1> 
         <h2>Paragrafo Daina</h2>
-        <p>Aš toks vienišas jaunas...</p>
-       <?php //<p>Mindau svetimo kodo gatves...</p> ?>
+     <?php // <p>Aš toks vienišas jaunas...</p>?>
+       <p>Mindau svetimo kodo gatves...</p>
     </body>
 </html>
