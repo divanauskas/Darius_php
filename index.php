2ac90641e4a078715bcf1f13a5f602125e42829d
Your NameDarius <didariusivanauskas@gmail.com>
2019-04-16 14:58:31 +0300

    Antras commit

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-print 'Veikia!';
+<!doctype html>
+<html>
+    <head>
+        <title>Aš valdau HTML!</title>
+    </head>
+    <body>
+        <h1>Vardas - HTML asas</h1>
+        <p>Moku kurti statinį turinį</p>
+        
+        
+    </body>
+    
+</html>
