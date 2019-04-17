67a53375ad7af09d534a25e3b5f9d4551d64ecb0
Your NameDarius <didariusivanauskas@gmail.com>
2019-04-17 16:15:39 +0300

    Nuotraukos

--- index.php
+++ index.php
@@ -1,51 +1,16 @@
+<?php
+ $image = '5.png';
+ $pic = '3.png';
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>PHP lydės ir <?php print date(rand(2019, 2029)) . "-" . date(rand(1, 12)); ?>!</title>
+        <title>PHP lydės</title>
     </head>
-    <body style="background-color: rgb(
-        
-        <?php 
-        
-        print rand(000, 255) . ', ' . rand(000, 255) . ', ' . rand(000, 255); 
-        
-        ?>
-          
-        ")>
-        
-        
-        <h1 style="font-size: 
-            <?php print rand(0, 100) . 'px'; ?>;">Aš keičiu savo dydį!</h1>
-            
-            
-            <p style="color: rgb(
-               
-               <?php
-               
-               print rand(000, 255) . ', ' . rand(000, 255) . ', ' . rand(000, 255);
-               
-               ?>)
-               
-               ">Aš keičiu savo spalvą</p>
-            
-          <img src="<?php print rand(1, 6); ?>.png"> 
-      
-        <h1>Glabūt turėsiu <?php print rand(1, 5); ?> vaikų(us)!</h1> 
-        <p>D. Trump'as nebebus prezidentu <?php print date(rand(2021, 2031)) . date(' M-d'); ?> </p>
-        
-                 
-    <?php
-        $a = 'Opa';
-        $b = '1';
-        $c = '2.53';
-        $d = 'true';
-        $e = 'null';
-
-        print $a;
-        print $b;
-        print $c;
-        print $d;
-        print $e;
-    ?>        
+    <body>
+        <img src="<?php print $pic ?>">
+        <img src="<?php print $image ?>">
+        <img src="<?php print $pic ?>">
+        <img src="<?php print $image ?>">
     </body>
 </html>
