4eb7398ac75a76fee0a84e5c91ac41f5b8377674
Your NameDarius <didariusivanauskas@gmail.com>
2019-04-17 16:51:14 +0300

    paskola

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
- $image = '5.png';
- $pic = '3.png';
+    $paskola = (rand(1, 100));
+    $palukanos = (rand(101, 200));
+    $palukaneles = (rand(201, 300));
 ?>
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>PHP lydės</title>
     </head>
     <body>
-        <img src="<?php print $pic ?>">
-        <img src="<?php print $image ?>">
-        <img src="<?php print $pic ?>">
-        <img src="<?php print $image ?>">
+        <div>
+            <h1>Skolos skaičiuoklė</h1>
+            <p>Jei paėmei <?php print $paskola;?> jievru</p><br>
+            <p>Su dviem kabančiais grąžinsi <?php print $palukaneles;?></p><br>
+            <p>Su vienu kabančiu grąžinsi <?php print $palukanos;?></p>
+        </div>   
     </body>
 </html>
