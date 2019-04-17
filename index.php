16fd82869a3b7673a46a0dbdb8a6790c2ccde443
Your NameDarius <didariusivanauskas@gmail.com>
2019-04-17 17:15:37 +0300

    Clean code

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-    $paskola = (rand(1, 100));
-    $palukanos = (rand(101, 200));
-    $palukaneles = (rand(201, 300));
+
+    $paskola = rand(1, 100);
+    $palukanos = rand(101, 200);
+    $palukaneles = rand(201, 300);
+    
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,11 +12,9 @@
         <title>PHP lydės</title>
     </head>
     <body>
-        <div>
-            <h1>Skolos skaičiuoklė</h1>
-            <p>Jei paėmei <?php print $paskola;?> jievru</p><br>
-            <p>Su dviem kabančiais grąžinsi <?php print $palukaneles;?></p><br>
-            <p>Su vienu kabančiu grąžinsi <?php print $palukanos;?></p>
-        </div>   
+        <h1>Skolos skaičiuoklė</h1>
+        <h2>Jei paėmei <?php print $paskola;?> jievru</h2>
+        <h3>Su dviem kabančiais grąžinsi <?php print $palukaneles;?></h3>
+        <h3>Su vienu kabančiu grąžinsi <?php print $palukanos;?></h3> 
     </body>
 </html>
